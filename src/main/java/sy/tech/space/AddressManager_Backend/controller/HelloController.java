f7014c375288501d8a5315a07fb00795a2145f57
Mohammad Assaf <mohammad.assaf@stud.hs-flensburg.de>
2024-03-18 21:58:26 +0100

    init project

---
+++ src/main/java/sy/tech/space/AddressManager_Backend/controller/HelloController.java
@@ -0,0 +1,12 @@
+package sy.tech.space.AddressManager_Backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
